96ee4ae3c5a6c9cfb46fe2354a50b7b1815d735f
Sahm <h4k4s4m@gmail.com>
2018-07-21 16:03:25 -0700

    Added the main window, chat button and added preliminary styles

--- content.js
+++ content.js
@@ -1,1 +1,13 @@
-alert("Hello! I am an alert box!!");
+// Creates and adds button to document body
+var Butt = $('<button/>',
+{
+    text: 'Chat',
+    click: function () { 
+        $(".main-window").toggle()
+     }
+});
+Butt.addClass("main-butt")
+$(document.body).append(Butt)
+
+var chat = $('<div style="display:none" class="main-window"> <div/>');
+$(document.body).append(chat)
